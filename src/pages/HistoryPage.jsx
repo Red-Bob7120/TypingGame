910c64876c8c1e1639603301d13e8f62e4b9a40e
Red-Bob7120 <pjh93100@gmail.com>
2025-12-06 15:03:06 +0900

    add button Hompage

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,8 +1,20 @@
+import {useNavigate} from "react-router-dom";
+
+
+
 function HistoryPage (){
+    const navigate = useNavigate();
+
+    const handleStart = () => {
+        navigate("/practice");
+    }
     return(
         <div style ={{padding:"20px"}}>
             <h1>HistoryPage</h1>
             <p>연습기록 저장 및 표시</p>
+
+            <button
+                onClick={handleStart}연습 시작하기></button>
         </div>
     )
 }
--- src/pages/PracticePage.jsx
+++ src/pages/PracticePage.jsx
@@ -3,6 +3,11 @@ export default function PracticePage(){
         <div style={{padding:"20px"}}>
             <h1>Practice Page</h1>
             <p>연습용 예문 및 입력창 추가</p>
+
+
+            <div>
+                <p>예문을 준비하는 중...</p>
+            </div>
         </div>
     )
 }
