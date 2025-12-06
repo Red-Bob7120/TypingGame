1d2ca134b57f041d6a6d8c8955c20e854b9c06fc
Red-Bob7120 <pjh93100@gmail.com>
2025-12-06 15:29:28 +0900

    feat: add snippets.json and load random snippet in PracticePage

--- src/pages/PracticePage.jsx
+++ src/pages/PracticePage.jsx
@@ -1,15 +1,28 @@
+import {useEffect,useState} from "react";
 import "../styles/Practicepage.css";
+import snippets from "../data/snippets.json";
 
 function PracticePage(){
+    const[snippet,setSnippet] = useState("");
+
+    useEffect(()=> {
+        const random = snippets[Math.floor(Math.random()*snippets.length)];
+        setSnippet(random.content);
+    },[])
     return(
         <div className="practice-container">
+
             <h1 className ="practice-title">Practice Page</h1>
+
             <p className="practice-desc">연습용 예문 및 입력창 추가</p>
-
-
             <div className="snippet-box">
-                <p>예문을 준비하는 중...</p>
+                <pre>{snippet}</pre>
             </div>
+
+            <textarea
+                className="input-area"
+                placeholder="여기에 입력해보세요..."
+            ></textarea>
         </div>
     )
 }
